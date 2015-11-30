651f288c3dcec1b25a3ee36b1210adc3e579d051
zhangmingkai4315 <zhangmingkai19892013@gmail.com>
2015-11-30 15:28:52 +0800

    表单的处理和 Flash消息的发送

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,session,url_for,flash
 from flask import request
 from flask import current_app
 from flask import make_response,redirect,abort
@@ -8,7 +8,14 @@ from datetime import datetime
 from flask.ext.script import Manager
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.moment import Moment
+# //form class
+from flask.ext.wtf import Form
+from wtforms import StringField,SubmitField
+from wtforms.validators import Required
 
+class NameForm(Form):
+    name=StringField('What is your name?',validators=[Required()])
+    submit=SubmitField('Submit')
 # ///////////////////////////
 
 print __name__
@@ -18,6 +25,19 @@ def index():
     user_agent=request.headers.get('User-Agent')
     # return '<h1>Hello world ! </h1><h3>{}</h3><h4>{}</h4>'.format(user_agent,current_app.name)
     return render_template('index.html')
+@app.route('/login',methods=['GET','POST'])
+def login():
+    form=NameForm()
+    if form.validate_on_submit():
+        oldname=session.get('user')
+        name=form.name.data
+        if(oldname is not None and oldname!=name):
+            flash('YOU HAVE CHANGED YOUR NAME FROM:{} TO {}'.format(oldname,name))
+        session['user']=name
+        return redirect(url_for('login'))
+        # name=form.name.data
+        # form.name.data=''
+    return render_template('login.html',form=form,name=session.get('user'))
 @app.route('/user/<name>')
 def user(name):
     if name=='mingkai':
@@ -44,6 +64,7 @@ def page_not_found(e):
     return render_template('500.html'),500
 if __name__ == '__main__':
     app.debug=True
+    app.config['SECRET_KEY']='Do not try to guess'
     bootstrap=Bootstrap(app)
     moment=Moment(app)
     manager=Manager(app)
