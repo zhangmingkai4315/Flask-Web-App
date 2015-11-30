178b15f21a68447dc9b7eda339d780659305b5a8
zhangmingkai4315 <zhangmingkai19892013@gmail.com>
2015-11-30 17:08:20 +0800

    Config Database connection and ops. Use flask-migration to migrate database hurtless

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import request
 from flask import current_app
 from flask import make_response,redirect,abort
 from datetime import datetime
+import os
 # ///////////////////////////
 
 from flask.ext.script import Manager
@@ -13,13 +14,53 @@ from flask.ext.wtf import Form
 from wtforms import StringField,SubmitField
 from wtforms.validators import Required
 
+from flask.ext.sqlalchemy import SQLAlchemy
+basedir=os.path.abspath(os.path.dirname(__file__))
+
+
+from flask.ext.migrate import Migrate,MigrateCommand
+
+
+# ///////////////////////////
+
+app=Flask(__name__)
+app.debug=True
+app.config['SECRET_KEY']='Do not try to guess'
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///'+os.path.join(basedir,'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN']=True
+
+db=SQLAlchemy(app)
+bootstrap=Bootstrap(app)
+moment=Moment(app)
+migrate=Migrate(app,db)
+
+
+
+
 class NameForm(Form):
     name=StringField('What is your name?',validators=[Required()])
     submit=SubmitField('Submit')
-# ///////////////////////////
 
-print __name__
-app=Flask(__name__)
+class Role(db.Model):
+    __tablename__='roles'
+    id=db.Column(db.Integer,primary_key=True)
+    name=db.Column(db.String(64),unique=True)
+    users=db.relationship('User',backref='role')
+    def __repr__(self):
+        return '<Role>{}</Role>'.format(self.name)
+class User(db.Model):
+    __tablename__='users'
+    id=db.Column(db.Integer,primary_key=True)
+    role_id=db.Column(db.Integer,db.ForeignKey('roles.id'))
+    username=db.Column(db.String(64),unique=True,index=True)
+    email=db.Column(db.String(64),nullable=True)
+    def __repr__(self):
+        return '<Username>{}</Username>'.format(self.username)
+
+
+
+
+
 @app.route('/')
 def index():
     user_agent=request.headers.get('User-Agent')
@@ -40,32 +81,47 @@ def login():
     return render_template('login.html',form=form,name=session.get('user'))
 @app.route('/user/<name>')
 def user(name):
-    if name=='mingkai':
-        # response=make_response('<h1> Hi %s</h1' % name)
-        # response.set_cookie('user',name)
-        # return response,200
-        return render_template('user.html',name=name,current_time=datetime.utcnow())
-    elif name=='admin':
-        # return redirect('/')
+    user=User.query.filter_by(username=name).first()
+    if user is None:
+        return redirect('/')
+    if user.role.name=='admin':
         return render_template('admin.html',name=name)
+    elif user.role.name=='user':
+        return render_template('user.html',name=name,current_time=datetime.utcnow())
     else:
-        abort(404)
-
+        return redirect('/')
+    # if name=='mingkai':
+    #     # response=make_response('<h1> Hi %s</h1' % name)
+    #     # response.set_cookie('user',name)
+    #     # return response,200
+    #     return render_template('user.html',name=name,current_time=datetime.utcnow())
+    # elif name=='admin':
+    #     # return redirect('/')
+    #     return render_template('admin.html',name=name)
+    # else:
+    #     abort(404)
 @app.route('/users')
 def userlist():
     userlist=['mike','alice','bob']
     return render_template('users.html',users=userlist)
-
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'),404
 @app.errorhandler(500)
 def page_not_found(e):
     return render_template('500.html'),500
+
+# //////////////
+
+
+
+
+
+
+# ////////////////
+
 if __name__ == '__main__':
-    app.debug=True
-    app.config['SECRET_KEY']='Do not try to guess'
-    bootstrap=Bootstrap(app)
-    moment=Moment(app)
+
     manager=Manager(app)
+    manager.add_command('db',MigrateCommand)
     manager.run()
