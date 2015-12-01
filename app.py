eeb3a3b5b0a3b7c3b0d62f8515ef0a3e882d57d9
zhangmingkai4315 <zhangmingkai19892013@gmail.com>
2015-12-01 20:32:18 +0800

    add email function and send email in async way!

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import current_app
 from flask import make_response,redirect,abort
 from datetime import datetime
 import os
+from threading import Thread
 # ///////////////////////////
 
 from flask.ext.script import Manager
@@ -19,6 +20,7 @@ basedir=os.path.abspath(os.path.dirname(__file__))
 
 
 from flask.ext.migrate import Migrate,MigrateCommand
+from flask.ext.mail import Mail,Message
 
 
 # ///////////////////////////
@@ -29,10 +31,37 @@ app.config['SECRET_KEY']='Do not try to guess'
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///'+os.path.join(basedir,'data.sqlite')
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN']=True
 
+app.config['SUBJECT_PREFIX']='[MIKE]'
+app.config['MAIL_SENDER']=os.environ.get('MAIL_ADMIN')
+app.config['MAIL_SERVER']='smtp.qq.com'
+app.config['MAIL_PORT']=587
+app.config['MAIL_USE_TLS']=True
+
+app.config['MAIL_USERNAME']=os.environ.get('MAIL_ADMIN')
+app.config['MAIL_PASSWORD']=os.environ.get('MAIL_PASSWORD')
+app.config['ADMIN']=os.environ.get('MAIL_ADMIN')
+
+def send_mail(to,subject,template,**kwargs):
+    msg=Message(app.config['SUBJECT_PREFIX']+subject,sender=app.config['MAIL_SENDER'],recipients=[to])
+    msg.body=render_template(template+'.txt',**kwargs)
+    msg.html=render_template(template+'.html',**kwargs)
+
+    thread_mail=Thread(target=send_async_mail,args=[app,msg])
+    thread_mail.start()
+    return thread_mail
+
+
+
+def send_async_mail(app,msg):
+    with app.app_context():
+        mail.send(msg)
+
+
 db=SQLAlchemy(app)
 bootstrap=Bootstrap(app)
 moment=Moment(app)
 migrate=Migrate(app,db)
+mail=Mail(app)
 
 
 
@@ -53,7 +82,6 @@ class User(db.Model):
     id=db.Column(db.Integer,primary_key=True)
     role_id=db.Column(db.Integer,db.ForeignKey('roles.id'))
     username=db.Column(db.String(64),unique=True,index=True)
-    email=db.Column(db.String(64),nullable=True)
     def __repr__(self):
         return '<Username>{}</Username>'.format(self.username)
 
@@ -85,6 +113,8 @@ def user(name):
     if user is None:
         return redirect('/')
     if user.role.name=='admin':
+        if app.config['ADMIN']:
+            send_mail(app.config['ADMIN'],'admin login','mail/login',user=user)
         return render_template('admin.html',name=name)
     elif user.role.name=='user':
         return render_template('user.html',name=name,current_time=datetime.utcnow())
